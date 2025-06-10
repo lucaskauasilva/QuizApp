7330b546b8ce7e55b12fd75dc280b75a87899f0f
Lucas <lukaskaua172003@gmail.com>
2025-06-10 15:27:23 -0300

    feat: adicionar o fragmento 'CadastrarFragment' e limpar as linhas de código inutilizáveis criadas automaticamente

---
+++ app/src/main/java/com/example/quizapp/CadastrarFragment.java
@@ -0,0 +1,36 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link CadastrarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class CadastrarFragment extends Fragment {
+
+    public CadastrarFragment() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_cadastrar, container, false);
+    }
+}
