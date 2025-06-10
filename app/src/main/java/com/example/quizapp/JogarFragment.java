66ee2660fd7f5617bd7b822ea757dc478e3a2c51
Lucas <lukaskaua172003@gmail.com>
2025-06-10 09:38:30 -0300

    feat: adicionar o fragmento 'JogarFragment' e limpar linhas inutilizáveis criadas automaticamente

---
+++ app/src/main/java/com/example/quizapp/JogarFragment.java
@@ -0,0 +1,36 @@
+package com.example.quizapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link JogarFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class JogarFragment extends Fragment {
+
+    public JogarFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_jogar, container, false);
+    }
+}
