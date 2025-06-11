9f2ca9c95ef92ec97d739e2920e39c06a80ca614
Lucas <lukaskaua172003@gmail.com>
2025-06-11 15:49:54 -0300

    feat: adicionar em JogarFragment.java o método onViewCreated para iniciar as implementações dos objetos do fragment_jogar.xml

--- app/src/main/java/com/example/quizapp/JogarFragment.java
+++ app/src/main/java/com/example/quizapp/JogarFragment.java
@@ -2,6 +2,8 @@ package com.example.quizapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -30,5 +32,10 @@ public class JogarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_jogar, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
