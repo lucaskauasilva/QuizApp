e313b85488b42f477baa23b74478d8da664fc40c
Lucas <lukaskaua172003@gmail.com>
2025-06-26 15:51:48 -0300

    feat: declarar na classe CadastrarFragment as views do fragment_jogar.xml, e iniciá-las no onViewCreated com findViewById para implementações futuras

--- app/src/main/java/com/example/quizapp/CadastrarFragment.java
+++ app/src/main/java/com/example/quizapp/CadastrarFragment.java
@@ -2,11 +2,15 @@ package com.example.quizapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -14,6 +18,11 @@ import android.view.ViewGroup;
  * create an instance of this fragment.
  */
 public class CadastrarFragment extends Fragment {
+
+    private EditText mEditTextPergunta;
+    private EditText mEditTextResposta;
+    private Button mButtonCadastrar;
+    private Button mButtonJogar;
 
     public CadastrarFragment() {
 
@@ -30,5 +39,19 @@ public class CadastrarFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cadastrar, container, false);
+
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+            mEditTextPergunta = getActivity().findViewById(R.id.etPergunta);
+            mEditTextResposta = getActivity().findViewById(R.id.etResposta);
+            mButtonCadastrar = getActivity().findViewById(R.id.btnCadastrar);
+            mButtonCadastrar = getActivity().findViewById(R.id.btnJogar);
+
+
+
     }
 }
