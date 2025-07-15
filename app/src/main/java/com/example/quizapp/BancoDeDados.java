084424a066563a0e21bb35f5fe27dd92976366bd
Lucas <lukaskaua172003@gmail.com>
2025-07-15 07:25:49 -0300

    feat: implementar método estático getBancoDeDados na classe BancoDeDados com Room

--- app/src/main/java/com/example/quizapp/BancoDeDados.java
+++ app/src/main/java/com/example/quizapp/BancoDeDados.java
@@ -1,6 +1,9 @@
 package com.example.quizapp;
 
+import android.content.Context;
+
 import androidx.room.Database;
+import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
 @Database(entities = {Questoes.class}, version = 1)
@@ -9,5 +12,18 @@ public abstract class BancoDeDados extends RoomDatabase{
     public abstract MeuDAO meuDAO(); // para recuperar o objeto DAO
 
     private static BancoDeDados INSTANCE;
+    
+    public static BancoDeDados getBancoDeDados(final Context context) {
+        if (INSTANCE == null) {
+            synchronized (BancoDeDados.class){
+                if (INSTANCE == null) {
+                    //cria o BD
+                    INSTANCE = Room.databaseBuilder(context.getApplicationContext(),
+                            BancoDeDados.class, "bd_questoes").allowMainThreadQueries().build();
+                }
+            }
+        }
+        return INSTANCE;
+    }
 
 }
