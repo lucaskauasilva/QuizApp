a8a86aad593122a00814d35db96993907d64e656
Lucas <lukaskaua172003@gmail.com>
2025-07-18 11:16:47 -0300

    feat: implementar classe Questoes com atributos id, pergunta e resposta, construtor e métodos getters e setters

--- app/src/main/java/com/example/quizapp/Questoes.java
+++ app/src/main/java/com/example/quizapp/Questoes.java
@@ -1,4 +1,26 @@
 package com.example.quizapp;
 
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity
 public class Questoes {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String pergunta;
+    private String resposta;
+
+    public Questoes(String pergunta, String resposta) {
+        this.pergunta = pergunta;
+        this.resposta = resposta;
+    }
+
+    /* Getters | Setters */
+    public int getId() { return id; }
+    public void setId(int id) { this.id = id; }
+    public String getPergunta() { return pergunta; }
+    public void setPergunta(String pergunta) { this.pergunta = pergunta; }
+    public String getResposta() { return resposta; }
+    public void setResposta(String resposta) { this.resposta = resposta; }
 }
