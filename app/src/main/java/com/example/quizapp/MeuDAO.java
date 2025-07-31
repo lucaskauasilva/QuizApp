f76701f95a89dfb42b961cbf7911087bda1d4b75
Lucas <lukaskaua172003@gmail.com>
2025-07-31 08:20:13 -0300

    feat: adicionar interface MeuDao com funções de persistência no banco — inserir, consultar e excluir registros

--- app/src/main/java/com/example/quizapp/MeuDAO.java
+++ app/src/main/java/com/example/quizapp/MeuDAO.java
@@ -1,7 +1,21 @@
 package com.example.quizapp;
 
 import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
 
 @Dao
 public interface MeuDAO {
+
+    @Insert
+    long inserirQuestoes(Questoes questoes);
+
+    @Query("SELECT * FROM Questoes")
+    List<Questoes> pesquisarTodasQuestoes();
+
+    @Query("DELETE FROM Questoes")
+    void apagarTabela();
+
 }
